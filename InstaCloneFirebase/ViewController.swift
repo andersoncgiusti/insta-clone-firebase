20555253edd5ba5f17fd0ca6dc46d6dbde46446a
Anderson  Giusti <ibm.anderson@grupofleury.com.br>
2023-03-13 18:05:59 -0300

    first commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Anderson Carvalho Giusti on 13/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
