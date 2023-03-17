da6bfaa312a8bf24714dfc15625b98ac5680fdfb
Anderson  Giusti <ibm.anderson@grupofleury.com.br>
2023-03-17 18:10:36 -0300

    auth

--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
     }
     
     @IBAction func signInClicked(_ sender: Any) {
